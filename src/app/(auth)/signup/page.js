86ea3eb0bb00128cc19023c7fe84dcebdd212f0f
ook-cloud <ooktb57@gmail.com>
2026-09-14 10:19:25 +0800

    mday

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import Image from "next/image";
-import { SignupForm } from "./components/SignUpForm.js";
+import { SignupForm } from "./features/signupfrom";
 
 export default function SignupPage() {
   const [step, setStep] = useState(1);
